58241b06cba4852ecf1810a1d106c50832600252
Anthony Hamlin <adhamlingithub@gmail.com>
2022-06-19 10:27:50 -0500

    Finished video 29 | https://youtu.be/lpcOSXWPXTk?list=PLqq-6Pq4lTTbx8p2oCgcAQGQyqN8XeA1x

--- src/main/java/com/example/springcourseapidata/topic/Topic.java
+++ src/main/java/com/example/springcourseapidata/topic/Topic.java
@@ -3,8 +3,12 @@ package com.example.springcourseapidata.topic;
 import jdk.nashorn.internal.objects.annotations.Getter;
 import jdk.nashorn.internal.objects.annotations.Setter;
 
-public class Topic {
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+@Entity
+public class Topic {
+    @Id
     private String id;
     private String name;
     private String description;
--- src/main/java/com/example/springcourseapidata/topic/TopicService.java
+++ src/main/java/com/example/springcourseapidata/topic/TopicService.java
@@ -1,14 +1,19 @@
 package com.example.springcourseapidata.topic;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 // singleton
 @Service
 public class TopicService {
+
+    @Autowired
+    private TopicRepository topicRepository;
 
     private List<Topic> topics = new ArrayList<>(Arrays.asList(  // hardcode dummy data - Spring will automatically convert it to JSON
             new Topic("Spring", "Spring Framework", "a description of the Spring Framework"),
@@ -18,33 +23,45 @@ public class TopicService {
     ));
 
     public List<Topic> getAllTopics() {
+        // return topics;
+        List<Topic> topics = new ArrayList<>();
+        topicRepository.findAll().forEach(topics::add);  //  method ref - lambda basics
         return topics;
     }
 
-    public Topic getTopic(String id) {
-        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();  // filter lambda expression
+    public Optional<Topic> getTopic(String id) {
+        // return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();  // filter lambda expression
+        return topicRepository.findById(id);
     }
 
     public void addTopic(Topic topic) {
-        topics.add(topic);
+        // topics.add(topic);
+        topicRepository.save(topic);
     }
 
     public void updateTopic(String id, Topic topic) {
-        for (int i = 0; i < topics.size(); i++) {
-            Topic t = topics.get(i);
-            if (t.getId().equals(id)) {
-                topics.set(i, topic);
-                return;
-            }
-        }
+//        for (int i = 0; i < topics.size(); i++) {
+//            Topic t = topics.get(i);
+//            if (t.getId().equals(id)) {
+//                topics.set(i, topic);
+//                return;
+//            }
+//        }
+        topicRepository.save(topic);
+
     }
 
     public Topic deleteTopic(String id) {
-        try {
-            topics.removeIf(t -> t.getId().equals(id));
-        } catch (Exception e) {
-            System.out.println(e + " Something went wrong!");
-        }
+//        try {
+//            topics.removeIf(t -> t.getId().equals(id));
+//        } catch (Exception e) {
+//            System.out.println(e + " Something went wrong!");
+//        }
+//        return null;
+
+        topicRepository.deleteById(id);
         return null;
     }
+
+
 }
